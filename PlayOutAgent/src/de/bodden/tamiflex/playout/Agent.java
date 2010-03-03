b97d5f8165a9667260efadcb35e6de5049490672
Eric Bodden <Eric Bodden@localhost>
2010-03-03 21:13:15 +0100

    added license message

--- PlayOutAgent/src/de/bodden/tamiflex/playout/Agent.java
+++ PlayOutAgent/src/de/bodden/tamiflex/playout/Agent.java
@@ -1,3 +1,13 @@
+/*******************************************************************************
+ * Copyright (c) 2010 Eric Bodden.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ *     Eric Bodden - initial API and implementation
+ ******************************************************************************/
 package de.bodden.tamiflex.playout;
 
 import java.io.File;
