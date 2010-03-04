d2f1d02cc1aa7dedabad95aec3a3f0bb70208408
Eric Bodden <Eric Bodden@localhost>
2010-03-04 14:20:56 +0100

    removing unnecessary flag

--- PlayOutAgent/rtlib/de/bodden/tamiflex/playout/rt/ReflLogger.java
+++ PlayOutAgent/rtlib/de/bodden/tamiflex/playout/rt/ReflLogger.java
@@ -212,7 +212,6 @@ public class ReflLogger {
 	}
 	
 	public static synchronized void writeLogfileToDisk() {
-		ShutdownStatus.hasShutDown = true;
 		Set<PersistedLogEntry> mergedEntries = mergeOldAndNewLog();
 		//printStatistics();
 		try {
