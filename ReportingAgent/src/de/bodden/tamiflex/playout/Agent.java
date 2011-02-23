11eccd169db221d484f1267bf695fbf4afc879c7
Eric Bodden <Eric Bodden@localhost>
2011-02-23 11:58:28 +0100

    initial import of reporter (spawned from POA)

---
+++ ReportingAgent/src/de/bodden/tamiflex/playout/Agent.java
@@ -0,0 +1,184 @@
+/*******************************************************************************
+ * Copyright (c) 2010 Eric Bodden.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ *     Eric Bodden - initial API and implementation
+ ******************************************************************************/
+package de.bodden.tamiflex.playout;
+
+import java.io.File;
+import java.io.IOException;
+import java.lang.instrument.Instrumentation;
+import java.lang.instrument.UnmodifiableClassException;
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Method;
+import java.net.Socket;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.util.jar.JarFile;
+
+import de.bodden.tamiflex.normalizer.Hasher;
+import de.bodden.tamiflex.playout.rt.ReflLogger;
+import de.bodden.tamiflex.playout.rt.ShutdownStatus;
+
+public class Agent {
+	
+	public final static String PKGNAME = Agent.class.getPackage().getName().replace('.', '/');
+	private static Socket socket;
+	
+	public static void premain(String agentArgs, Instrumentation inst) throws IOException, ClassNotFoundException, UnmodifiableClassException, URISyntaxException, InterruptedException {
+		if(!inst.isRetransformClassesSupported()) {
+			throw new RuntimeException("retransformation not supported");
+		}
+		
+		if(agentArgs==null) agentArgs = "";
+		boolean dontNormalize = false;
+		if(agentArgs.startsWith("dontNormalize,")) {
+			dontNormalize = true;
+			agentArgs = agentArgs.substring("dontNormalize,".length());
+		}
+		boolean count = false;
+		if(agentArgs.startsWith("count,")) {
+			count = true;
+			agentArgs = agentArgs.substring("count,".length());
+		}
+		boolean verbose = false;
+		if(agentArgs.startsWith("verbose,")) {
+			verbose = true;
+			agentArgs = agentArgs.substring("verbose,".length());
+		}
+		boolean useSocket = false;
+		if(agentArgs.startsWith("socket,")) {
+			useSocket = true;
+			agentArgs = agentArgs.substring("socket,".length());
+		}
+		if(agentArgs.equals("")) usage();
+		
+		appendRtJarToBootClassPath(inst);
+
+		ReflLogger.setMustCount(count);		
+		if(dontNormalize) Hasher.dontNormalize();
+
+		if(useSocket) {
+			//online mode; no need to create any files; just insert instrumentation...
+			String hostColonPort = agentArgs;
+			if(!hostColonPort.contains(":")) throw new IllegalArgumentException("Wrong destination "+hostColonPort+" ! Format is host:port.");
+			String[] split = hostColonPort.split(":");
+			String host = split[0];
+			int port = Integer.parseInt(split[1]);
+			socket = new Socket(host, port);
+			ReflLogger.setSocket(socket);
+			instrumentClassesForLogging(inst);
+		} else {
+			String outPath=agentArgs;
+			if(outPath==null||outPath.isEmpty()) {
+				System.err.println("No outpath given!");
+				usage();
+			}
+			
+			File outDir = new File(outPath);
+			if(outDir.exists()) {
+				if(!outDir.isDirectory()) {
+					System.err.println(outDir+ "is not a directory");
+					usage();
+				}
+			} else {
+				boolean res = outDir.mkdirs();
+				if(!res) {
+					System.err.println("Cannot create directory "+outDir);
+					usage();
+				}
+			}
+			
+			final File logFile = new File(outDir,"refl.log");
+					
+			ReflLogger.setLogFile(logFile);
+			
+			instrumentClassesForLogging(inst);
+			
+			final boolean verboseOutput = verbose;
+			Runtime.getRuntime().addShutdownHook(new Thread() {
+				
+				@Override
+				public void run() {
+					ShutdownStatus.hasShutDown = true;
+					if(socket!=null) {
+						try {
+							socket.close();
+						} catch (IOException e) {
+							e.printStackTrace();
+						}
+					}
+					ReflLogger.writeLogfileToDisk(verboseOutput);
+				}
+				
+			});
+		}
+}
+
+
+	private static void instrumentClassesForLogging(Instrumentation inst) throws UnmodifiableClassException {
+		ReflectionMonitor reflMonitor = new ReflectionMonitor();
+		inst.addTransformer(reflMonitor, true /* can retransform */);				
+
+		//make sure that these classes are instrumented
+		inst.retransformClasses(Class.class,Method.class,Constructor.class);
+		
+		//remove transformer again
+		inst.removeTransformer(reflMonitor);
+	}
+
+	private static void appendRtJarToBootClassPath(Instrumentation inst) throws URISyntaxException, IOException {
+		URL locationOfAgent = Agent.class.getResource("/de/bodden/tamiflex/playout/rt/ReflLogger.class");
+		if(locationOfAgent==null) {
+			System.err.println("Support library for reflection log not found on classpath.");
+			System.exit(1);
+		}
+		agentJarFilePath = locationOfAgent.getPath().substring(0, locationOfAgent.getPath().indexOf("!"));		
+		URI uri = new URI(agentJarFilePath);
+		JarFile jarFile = new JarFile(new File(uri));
+		inst.appendToBootstrapClassLoaderSearch(jarFile);
+	}
+
+	private static void usage() {
+		System.out.println("TamiFlex version "+Agent.class.getPackage().getImplementationVersion()+", Play-Out Agent \n");
+		System.out.println("This agent accepts the following options:");
+		System.out.println("[dontDumpClasses,][dontNormalize,][count,][verbose,]<path>");
+		System.out.println();
+		System.out.println("If 'dontDumpClasses' is given then the agent only produces a log file but dumps no classes.");
+		System.out.println("If 'dontNormalize' is given then the agent will not normalize randomized class names.");
+		System.out.println("If 'count' is selected then the agent will add the number of reflective invocations");
+		System.out.println("to the end of each line of the trace file.");
+		System.out.println("If 'verbose' is selected then the agent will print out all entries that it also added");
+		System.out.println("to the log file for the current run.");
+		System.out.println("");
+		System.out.println("The 'path' points to the output directory. The agent will write all class files");
+		System.out.println("into this directory. In addition, the agent will write a log file 'refl.log'. If this");
+		System.out.println("file already exists in the 'outpath' directory then the agent will add to the log,");
+		System.out.println("incrementing the respective counts, etc.");
+		System.out.println("");
+		System.out.println("");
+		System.out.println("For instance, the following command will cause the agent to dump class files into");
+		System.out.println("the directory /tmp/out, counting reflective invocations:");
+		System.out.println("java -javaagent:agent.jar=count,outpath=/tmp/out ...");
+		System.out.println(DISCLAIMER);
+		System.exit(1);
+	}
+	
+	private final static String DISCLAIMER=
+		"Copyright (c) 2010 Eric Bodden.\n" +
+		"\n" +
+		"DISCLAIMER: USE OF THIS SOFTWARE IS AT OWN RISK.\n" +
+		"\n" +
+		"All rights reserved. This program and the accompanying materials\n" +
+		"are made available under the terms of the Eclipse Public License v1.0\n" +
+		"which accompanies this distribution, and is available at\n" +
+		"http://www.eclipse.org/legal/epl-v10.html";
+	private static String agentJarFilePath;
+
+}
---
+++ ReportingAgent/src/de/bodden/tamiflex/playout/ReflectionMonitor.java
@@ -0,0 +1,153 @@
+/*******************************************************************************
+ * Copyright (c) 2010 Eric Bodden.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ *     Eric Bodden - initial API and implementation
+ ******************************************************************************/
+package de.bodden.tamiflex.playout;
+import static org.objectweb.asm.Opcodes.ALOAD;
+import static org.objectweb.asm.Opcodes.INVOKESTATIC;
+
+import java.lang.instrument.ClassFileTransformer;
+import java.lang.instrument.IllegalClassFormatException;
+import java.security.ProtectionDomain;
+
+import org.objectweb.asm.ClassAdapter;
+import org.objectweb.asm.ClassReader;
+import org.objectweb.asm.ClassVisitor;
+import org.objectweb.asm.ClassWriter;
+import org.objectweb.asm.MethodAdapter;
+import org.objectweb.asm.MethodVisitor;
+import org.objectweb.asm.Opcodes;
+
+import de.bodden.tamiflex.normalizer.NameExtractor;
+
+public class ReflectionMonitor implements ClassFileTransformer {
+	
+	public byte[] transform(ClassLoader loader, String className,
+		Class<?> classBeingRedefined, ProtectionDomain protectionDomain,
+		byte[] classfileBuffer) throws IllegalClassFormatException {
+		if(className==null) {
+			className = NameExtractor.extractName(classfileBuffer);
+		}
+		final String theClassName = className;
+		
+		if(!className.equals("java/lang/Class") && !className.equals("java/lang/reflect/Method") && !className.equals("java/lang/reflect/Constructor")) return null;		
+		
+        try {
+        	// scan class binary format to find fields for toString() method
+        	ClassReader creader = new ClassReader(classfileBuffer);
+        	ClassWriter writer = new ClassWriter(ClassWriter.COMPUTE_MAXS);
+        	
+            ClassVisitor visitor = new ClassAdapter(writer) {
+            	
+            	public MethodVisitor visitMethod(int access, String methodName, String desc, String signature, String[] exceptions) {
+            		//delegate
+            		MethodVisitor mv = cv.visitMethod(access, methodName, desc, signature, exceptions);
+            		if(theClassName.equals("java/lang/Class") && methodName.equals("forName")) {
+            			mv = new ClassForNameAdapter(mv);            			
+            		} else if(theClassName.equals("java/lang/Class") && methodName.equals("newInstance0")) {
+            			mv = new ClassNewInstanceAdapter(mv);            			
+            		} else if(theClassName.equals("java/lang/reflect/Method") && methodName.equals("invoke")) {
+            			mv = new MethodInvokeAdapter(mv);            			
+            		} else if(theClassName.equals("java/lang/reflect/Constructor") && methodName.equals("newInstance")) {
+            			mv = new ConstructorNewInstanceAdapter(mv);            			
+            		}
+
+            		return mv;
+            	};
+            	
+            };
+            creader.accept(visitor, ClassReader.SKIP_DEBUG | ClassReader.SKIP_FRAMES);
+            return writer.toByteArray();
+        } catch (IllegalStateException e) {
+            throw new IllegalClassFormatException("Error: " + e.getMessage() +
+                " on class " + className);
+		} catch(RuntimeException e) {
+			e.printStackTrace();
+			throw e;
+		}
+	}
+	
+	static class ClassForNameAdapter extends MethodAdapter {
+
+		public ClassForNameAdapter(MethodVisitor mv) {
+			super(mv);
+		}
+		
+		@Override
+		public void visitInsn(int opcode) {
+			if(opcode==Opcodes.ARETURN) {
+    			//load first argument on stack, i.e. the name of the class to be loaded
+    			mv.visitVarInsn(ALOAD, 0);
+    			//call logging method with that Class object as argument
+				mv.visitMethodInsn(INVOKESTATIC, "de/bodden/tamiflex/playout/rt/ReflLogger", "classForName", "(Ljava/lang/String;)V");
+			}
+			super.visitInsn(opcode);
+		}
+		
+	}
+
+	static class ClassNewInstanceAdapter extends MethodAdapter {
+
+		public ClassNewInstanceAdapter(MethodVisitor mv) {
+			super(mv);
+		}
+		
+		@Override
+		public void visitInsn(int opcode) {
+			if(opcode==Opcodes.ARETURN) {
+    			//load "this" on stack, i.e. the Class object
+    			mv.visitVarInsn(ALOAD, 0);
+    			//call logging method with that Class object as argument
+				mv.visitMethodInsn(INVOKESTATIC, "de/bodden/tamiflex/playout/rt/ReflLogger", "classNewInstance", "(Ljava/lang/Class;)V");
+			}
+			super.visitInsn(opcode);
+		}
+		
+	}
+
+	static class MethodInvokeAdapter extends MethodAdapter {
+
+		public MethodInvokeAdapter(MethodVisitor mv) {
+			super(mv);
+		}
+		
+		@Override
+		public void visitInsn(int opcode) {
+			if(opcode==Opcodes.ARETURN) {
+    			//load first parameter on the stack, i.e. the designated receiver object
+    			mv.visitVarInsn(ALOAD, 1);
+    			//load "this" on stack, i.e. the Method object
+    			mv.visitVarInsn(ALOAD, 0);
+    			//call logging method with that Method object as argument
+				mv.visitMethodInsn(INVOKESTATIC, "de/bodden/tamiflex/playout/rt/ReflLogger", "methodInvoke", "(Ljava/lang/Object;Ljava/lang/reflect/Method;)V");
+			}
+			super.visitInsn(opcode);
+		}
+		
+	}
+	
+	static class ConstructorNewInstanceAdapter extends MethodAdapter {
+
+		public ConstructorNewInstanceAdapter(MethodVisitor mv) {
+			super(mv);
+		}
+		
+		@Override
+		public void visitInsn(int opcode) {
+			if(opcode==Opcodes.ARETURN) {
+    			//load "this" on stack, i.e. the Constructor object
+    			mv.visitVarInsn(ALOAD, 0);
+    			//call logging method with that Method object as argument
+				mv.visitMethodInsn(INVOKESTATIC, "de/bodden/tamiflex/playout/rt/ReflLogger", "constructorNewInstance", "(Ljava/lang/reflect/Constructor;)V");
+			}
+			super.visitInsn(opcode);
+		}
+		
+	}
+}
