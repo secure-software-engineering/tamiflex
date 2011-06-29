bb6b9c67036581a5daef122d47dbf14b7fcf0bfc
Eric Bodden <Eric Bodden@localhost>
2011-06-29 15:18:10 +0200

    configured PIA to use properties files as well

--- PlayInAgent/src/de/bodden/tamiflex/playin/Agent.java
+++ PlayInAgent/src/de/bodden/tamiflex/playin/Agent.java
@@ -10,11 +10,16 @@
  ******************************************************************************/
 package de.bodden.tamiflex.playin;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.io.InputStream;
 import java.lang.instrument.IllegalClassFormatException;
 import java.lang.instrument.Instrumentation;
 import java.lang.instrument.UnmodifiableClassException;
 import java.net.URISyntaxException;
+import java.util.Properties;
 
 import de.bodden.tamiflex.normalizer.Hasher;
 
@@ -25,30 +30,30 @@ public class Agent {
 	
 	public final static String PKGNAME = Agent.class.getPackage().getName().replace('.', '/');
 
+	private static String inPath = "out";
+	private static boolean verbose = false;
+
 	public static void premain(String agentArgs, Instrumentation inst) throws IOException, ClassNotFoundException, UnmodifiableClassException, URISyntaxException, IllegalClassFormatException {
-		if(agentArgs==null) agentArgs = "";
-		boolean verbose = false;
-		if(agentArgs.startsWith("verbose,")) {
-			verbose = true;
-			agentArgs = agentArgs.substring("verbose,".length());
-		}
-		if(agentArgs.startsWith("dontNormalize,")) {
-			agentArgs = agentArgs.substring("dontNormalize,".length());
-			Hasher.dontNormalize();
-		}
-		if(agentArgs.equals("")) usage();
-		final ClassReplacer replacer = new ClassReplacer(agentArgs,verbose);
+		
+		System.out.println("=======================================================");
+		System.out.println("TamiFlex Play-In Agent Version "+Agent.class.getPackage().getImplementationVersion());
+		loadProperties();
+		
+		final ClassReplacer replacer = new ClassReplacer(inPath,verbose);
 		inst.addTransformer(replacer,true);
 		
 		Runtime.getRuntime().addShutdownHook(new Thread() {
 			@Override
 			public void run() {
-				System.err.println("\n=============================================");
-				System.err.println("TamiFlex Play-In Agent Version "+Agent.class.getPackage().getImplementationVersion());
-				System.err.println("Replaced "+replacer.numSuccess+" out of "+replacer.numInvoked+" classes.");
+				System.out.println("\n=======================================================");
+				System.out.println("TamiFlex Play-In Agent Version "+Agent.class.getPackage().getImplementationVersion());
+				System.out.println("Replaced "+replacer.numSuccess+" out of "+replacer.numInvoked+" classes.");
+				System.out.println("=======================================================");
 			}
 		});
 		
+		System.out.println("=======================================================");
+
 		for (Class<?> c : inst.getAllLoadedClasses()) {
 			if(inst.isModifiableClass(c)) {
 				inst.retransformClasses(c);
@@ -56,25 +61,65 @@ public class Agent {
 				//warn if there is a class that we cannot re-transform, except for classes that resemble primitive types,
 				//arrays or are in java.lang
 				if(!c.isPrimitive() && !c.isArray() && (c.getPackage()==null || !c.getPackage().getName().startsWith("java.lang"))){
-					System.err.println("WARNING: Cannot replace class "+c.getName());
+					System.out.println("WARNING: Cannot replace class "+c.getName());
 				}
 			}
 		}
 	}
+	
+	private static void loadProperties() {
+		String propFileName = "pia.properties";
+		String[] paths = { propFileName, System.getProperty("user.home")+File.separator+".tamiflex"+File.separator+propFileName };
+		InputStream is = null;
+		File foundFile= null;
+		for (String path : paths) {
+			File file = new File(path);
+			if(file.exists() && file.canRead()) {
+				try {
+					is = new FileInputStream(file);
+					foundFile = file;
+					break;
+				} catch (FileNotFoundException e) {
+					e.printStackTrace();
+				}
+			} 
+		}
+		if(is==null) {
+			is = Agent.class.getClassLoader().getResourceAsStream(propFileName);
+			if(is==null) {
+				throw new InternalError("No default properties file found in agent JAR file!");
+			}
+		}
+		
+		Properties props =  new Properties();
+		try {
+			props.load(is);
+
+			if(!props.containsKey("quiet") || !props.get("quiet").equals("true")) {
+				String path = (foundFile!=null) ? foundFile.getAbsolutePath() : "<JAR FILE>!/"+propFileName;
+				System.out.println("Loaded properties from "+path);
+			}
+			if(props.get("dontNormalize").equals("true"))
+				Hasher.dontNormalize();
+			if(props.get("verbose").equals("true"))
+				verbose = true;
+			if(props.containsKey("outDir"))
+				inPath = (String) props.get("outDir"); 
+
+		} catch (IOException e) {
+			throw new InternalError("Error loading default properties file: "+e.getMessage()); 
+		}		
+	}
+	
+	public static void main(String[] args) {
+		usage();
+	}
 
 	private static void usage() {
-		System.out.println("TamiFlex version "+Agent.class.getPackage().getImplementationVersion()+", Play-in Agent \n");
-		System.out.println("This agent accepts the following options:");
-		System.out.println("[verbose,]<path>");
-		System.out.println();
-		System.out.println("If 'verbose' is given, then the replace agent will issue a warning when a ");
-		System.out.println("class cannot be found on the given path.");
-		System.out.println("If 'dontNormalize' is given then the agent will not normalize randomized class names.");
-		System.out.println("");
-		System.out.println("For instance, the following command will cause the agent to load class files from");
-		System.out.println("the directory /tmp/classes:");
-		System.out.println("java -javaagent:agent.jar=/tmp/classes ...");
+		System.out.println("============================================================");
+		System.out.println("TamiFlex Play-In Agent Version "+Agent.class.getPackage().getImplementationVersion());
 		System.out.println(DISCLAIMER);
+		System.out.println("============================================================");
 		System.exit(1);
 	}
 	
