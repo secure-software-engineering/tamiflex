8a7e5eb11b14e73a62128beca8f673567d4d4615
Eric Bodden <Eric Bodden@localhost>
2011-08-04 19:21:02 +0200

    run-configuration history

---
+++ de.bodden.tamiflex/src/de/bodden/tamiflex/action/PlayInToolbarAction.java
@@ -0,0 +1,23 @@
+/*******************************************************************************
+/*******************************************************************************
+ * Copyright (c) 2011 Eric Bodden.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ *     Eric Bodden - initial API and implementation
+ ******************************************************************************/
+package de.bodden.tamiflex.action;
+
+
+import org.eclipse.debug.ui.actions.AbstractLaunchToolbarAction;
+
+public class PlayInToolbarAction extends AbstractLaunchToolbarAction {
+
+	public PlayInToolbarAction() {
+		super("de.bodden.tamiflex.launchGroups.playin");
+	}
+
+}
